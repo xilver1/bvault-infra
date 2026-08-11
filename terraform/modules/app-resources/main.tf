08dadc0769b421f1d822272755ef1dae5bd50cb3
Giron <hiron@hiron.com>
2026-08-11 14:48:09 +0200

    Removed KMS encryption from repo to be replaced by AWS managed

--- terraform/modules/app-resources/main.tf
+++ terraform/modules/app-resources/main.tf
@@ -2,10 +2,6 @@ resource "aws_ecr_repository" "bvault_app_repo" {
   name                 = "bvault_app"
   image_tag_mutability = "IMMUTABLE_WITH_EXCLUSION"
 
-  encryption_configuration {
-    encryption_type = "KMS"
-  }
-
   image_tag_mutability_exclusion_filter {
     filter      = "latest*"
     filter_type = "WILDCARD"
